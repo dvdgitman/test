eeb97b21e10e4fc42af0c548fca910fcdcd58665
David Gitman <wereking@gmail.com>
2025-12-29 21:53:13 +0200

    RabbitMQ and Ingress Controller

---
+++ terraform/helm.tf
@@ -0,0 +1,50 @@
+resource "helm_release" "csi_secrets_store" {
+  name       = "csi-secrets-store"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+  namespace  = "kube-system"
+}
+
+resource "helm_release" "aws_secrets_provider" {
+  name       = "aws-secrets-provider"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "secrets-store-csi-driver-provider-aws"
+  namespace  = "kube-system"
+}
+
+resource "helm_release" "rabbitmq" {
+  name       = "rabbitmq"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "rabbitmq"
+  
+  set {
+    name  = "auth.username"
+    value = "user"
+  }
+
+  set {
+    name  = "auth.password"
+    value = var.rabbitmq_password 
+  }
+}
+
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller" 
+    # Note: Requires IAM role creation in iam.tf
+  }
+}
