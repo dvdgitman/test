acda503471f89d5abd59cf9c162d0b837c6ee1ff
David Gitman <wereking@gmail.com>
2026-01-04 03:28:23 +0200

    Add Helm release for CSI Secrets Store

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,3 +1,10 @@
+resource "helm_release" "csi_secrets_store" {
+  name       = "csi-secrets-store"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+  namespace  = "kube-system"
+}
+
 resource "helm_release" "aws_secrets_provider" {
   name       = "aws-secrets-provider"
   repository = "https://aws.github.io/eks-charts"
