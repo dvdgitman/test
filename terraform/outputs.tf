0cb684303227363238141071376e824d5f83280e
David Gitman <wereking@gmail.com>
2026-01-04 03:23:14 +0200

    Delete kubeconfig_command output
    Removed kubeconfig_command output from outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,3 @@ output "db_endpoint" {
   description = "The connection endpoint for the RDS database"
   value       = aws_db_instance.default.endpoint
 }
-
-output "kubeconfig_command" {
-  description = "Command to update local kubeconfig"
-  value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
-}
